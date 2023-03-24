a999393551622077296ee6c5c7635a7e93c7c9ea
Arie <tarmizi.arie@gmail.com>
2023-03-24 22:45:29 +0700

    initial commit

--- api/controllers/AccountController.php
+++ api/controllers/AccountController.php
@@ -4,76 +4,92 @@
 namespace api\controllers;
 
 
-use api\actions\ListAction;
 use api\components\Controller;
-use api\components\Response;
 use api\config\ApiCode;
-use api\filters\ContentTypeFilter;
-use common\models\Shop;
+use api\forms\user\CreateSubUserForm;
+use api\forms\user\SubscribeForm;
+use api\forms\user\SubscriptionCancelPaymentForm;
+use Carbon\Carbon;
+use common\models\Package;
+use common\models\PackageUser;
+use common\models\payment\PaymentInquiry;
+use common\models\SubscriptionUser;
 use common\models\User;
+use nadzif\base\rest\actions\FormAction;
+use nadzif\base\rest\components\Response;
+use Yii;
 use yii\helpers\ArrayHelper;
-use yii\web\NotFoundHttpException;
 
 class AccountController extends Controller
 {
-    public function behaviors()
-    {
-        $behaviors                        = parent::behaviors();
-        $behaviors['content-type-filter'] = [
-            'class'       => ContentTypeFilter::class,
-            'contentType' => ContentTypeFilter::TYPE_APPLICATION_JSON,
-            'only'        => [
-                'profile',
-            ],
-        ];
-
-        return $behaviors;
-    }
+    public $contentFilterActions = ['store', 'create-sub-user', 'subscribe', 'subscribe-cancel-payment'];
 
     public function actions()
     {
         return [
-            'profile' => [
-                'class'             => ListAction::class,
-                'query'             => function () {
-                    return User::find()
-                        ->where([User::tableName() . '.id' => \Yii::$app->user->id])
-                        ->joinWith(['shop']);
-                },
-                'toArrayProperties' => [
-                    User::class => [
-                        'id',
-                        'identityCardNumber',
-                        'name',
-                        'phoneNumber',
-                        'email',
-                        'birthDate',
-                        'address',
-                        'type',
-                        'status',
-//                        'marketplaceShopId' => function ($model) {
-//                            /** @var User $model */
-//                            return
-//                                $model->shop->marketplaceShopId;
-//                        },
-                        'shop' => function ($model) {
-                            return ArrayHelper::toArray($model->shop, [
-                                Shop::class => [
-                                    'marketplaceShopId',
-                                    'shopName',
-                                    'description',
-                                    'isOpen'
-                                ]
-                            ]);
-                        }
-                    ]
-                ],
-                'apiCodeSuccess'    => 0,
-                'apiCodeFailed'     => 400,
-                'successMessage'    => \Yii::t('app', 'Get user profile success'),
-            ]
         ];
     }
+
+    public function actionProfile()
+    {
+        /** @var User $user */
+        $user = \Yii::$app->user->identity;
+
+        $responseData = ArrayHelper::toArray($user, [
+            User::class => [
+                'id',
+                'code',
+                'identityCardNumber',
+                'name',
+                'phoneNumber',
+                'email',
+                'birthDate',
+                'status',
+                'verified',
+                'isParent' => function ($model) {
+                    /** @var User $model */
+                    if ($model->parentId == "" || $model->parentId == null) {
+                        return true;
+                    }
+
+                    return false;
+                },
+                'totalOrder' => function ($model) {
+                    /** @var User $model */
+                    return $model->getTotalOrderPerMonth();
+                },
+                'isAlertOrder' => function ($model) {
+                    /** @var User $model */
+
+//                    if ($model->subscription) {
+//                        $totalOrder = (int)$model->getTotalOrderPerMonth();
+//                        $alertOrder = (int)$model->subscription->maxOrder * ($model->subscription->alertOrderPercentage / 100);
+//
+//                        if (($model->subscription->maxOrder != -1) && ($totalOrder >= $alertOrder)) {
+//                            return true;
+//                        }
+//                    }
+
+                    return false;
+                },
+                'remainingOrder' => function ($model) {
+                    /** @var User $model */
+                    return $model->remainingOrder;
+                },
+            ]
+        ]);
+
+        $response = new Response();
+        $response->name = \Yii::t('app', 'Get Account Profile');
+        $response->message = \Yii::t('app', 'Get Account Profile Success');
+        $response->code = ApiCode::DEFAULT_SUCCESS_CODE;
+        $response->status = 200;
+        $response->data = $responseData;
+        $response->meta = [];
+
+        return $response;
+    }
+
 
     protected function verbs()
     {
